58f03420ca1d599c411e6ae1b38158a69b4cff54
Khalid <shkhalid95@mail.ru>
2023-09-18 17:14:46 +0300

    refactor collection names

--- src/contracts.ts
+++ src/contracts.ts
@@ -4,6 +4,6 @@ export type Bot = Telegraf;
 export type BotContext = Context;
 
 export enum CollectionsEnum {
-  'FOOTER' = 'welcome-bot_footer',
-  'CHAT_SETTINGS' = 'welcome-bot_chat-settings',
+  'FOOTER' = 'wb-chat-settings',
+  'CHAT_SETTINGS' = 'wb-chat-footer',
 }
